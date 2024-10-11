230fe0cf2f369e474dec384f38d8343d99db7ad8
Zarran <live.zaran@gmail.com>
2024-10-11 18:34:11 +0100

    setup the pan behavior

--- expo-bottomSeet/components/BottomSheet.tsx
+++ expo-bottomSeet/components/BottomSheet.tsx
@@ -1,33 +1,73 @@
-import { View, Text, Dimensions } from "react-native";
-import React from "react";
+import { View, Text, Dimensions, StatusBar } from "react-native";
+import React, { useEffect } from "react";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-const { width, height } = Dimensions.get("screen");
+const { width, height } = Dimensions.get("window");
+const Y_MAX_H = -height + (StatusBar.currentHeight || 100);
 
 const BottomSheet = () => {
+  // height shred value
+  const translateY = useSharedValue(0);
+  const context = useSharedValue({ y: 0 });
+
+  // gesture handler
+  const gusture = Gesture.Pan()
+    .onStart(() => {
+      // context.value = { y: translateY.value };
+    })
+    .onChange((event) => {
+      // translateY.value = event.translationY + context.value.y;
+      // translateY.value = Math.max(translateY.value, -height + (StatusBar.currentHeight || 50));
+      translateY.value += event.changeY;
+      translateY.value = Math.max(translateY.value, Y_MAX_H);
+    });
+
+  // animated style
+  const translateYStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  });
+
+  // applay animation on the start
+  useEffect(() => {
+    translateY.value = withSpring(-height * 0.3, { damping: 50 });
+  }, []);
+
   return (
-    // bottomSheet container
-    <View
-      style={{
-        backgroundColor: "white",
-        position: "absolute",
-        width,
-        height,
-        top: height * 0.6,
-        borderRadius: 20,
-      }}
-    >
-      {/* gray line */}
-      <View
-        style={{
-          backgroundColor: "gray",
-          alignSelf: "center",
-          width: 80,
-          height: 5,
-          borderRadius: 2.5,
-          marginVertical: 10,
-        }}
-      ></View>
-    </View>
+    <GestureDetector gesture={gusture}>
+      {/* bottomSheet container */}
+      <Animated.View
+        style={[
+          {
+            backgroundColor: "white",
+            position: "absolute",
+            width,
+            height,
+            top: height,
+            borderRadius: 20,
+          },
+          translateYStyle, // animated style
+        ]}
+      >
+        {/* gray line */}
+        <View
+          style={{
+            backgroundColor: "gray",
+            alignSelf: "center",
+            width: 80,
+            height: 5,
+            borderRadius: 2.5,
+            marginVertical: 10,
+          }}
+        />
+      </Animated.View>
+    </GestureDetector>
   );
 };
 
